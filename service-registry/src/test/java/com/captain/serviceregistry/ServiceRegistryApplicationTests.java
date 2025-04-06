e0ef2f55b49bc22d353ac363d0143e748e055330
Ritesh Kumar Singh <54631985+Riteshsingh13@users.noreply.github.com>
2025-04-06 10:07:35 +0530

    Service registry for microservices.

---
+++ service-registry/src/test/java/com/captain/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.captain.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
