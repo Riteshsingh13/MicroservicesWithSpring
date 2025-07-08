1bd0ef5bd46e1b3075362bbd125c2389adeee8a2
13sin <13sin@192.168.1.49>
2025-07-08 16:12:23 +0530

    implemented reduce quantity method.

--- ProductService/src/main/java/com/captain/ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com/captain/ProductService/service/ProductServiceImpl.java
@@ -46,4 +46,20 @@ public class ProductServiceImpl implements ProductService{
 		return productResponse;
 	}
 
+	@Override
+	public void reduceQuantity(long productId, long quantity) {
+		log.info("reduce quantity {} for id: {}", productId, quantity);
+		
+		Product product = productRepository.findById(productId)
+				.orElseThrow(() -> new ProductServiceCustomException("Product with given Id not found", "PRODUCT_NOT_FOUND")); 
+		
+		if(product.getQuantity() < quantity) {
+			throw new ProductServiceCustomException("Product doesn't have sufficient quantity", "INSUFFICIENT_QUANTITY");
+		}
+		
+		product.setQuantity(product.getQuantity() - quantity);
+		productRepository.save(product);
+		log.info("Product quantity updated successfully");
+	}
+
 }
